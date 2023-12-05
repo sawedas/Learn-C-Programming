720295c634a8fa8e8617cdb01261fe1662039348
sawedas <tar.tachit@Tachits-MacBook-Air.local>
2023-12-05 16:40:49 +0700

    variable

---
+++ Learn/variable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h> //import boolean
+
+int main()
+{
+    //type name_variable = start
+    int age = 19; //name_variable first charecter not number or special charecter unless _ (underscore)
+    char gender = 'm';//upper and lower character are difference meaning = case sensitive
+    char Gender = 'M';
+    float gpa = 3.86;
+    //char name_variable[amount of charecter] = "string" <-- How make variable keep string data?
+    char name[6] = "Tachit";
+    char surname[10] = "Julamphan", nick_name[3] = "Tar";
+    bool status = true;
+    //change data in existing variable
+    gpa = 3.96;
+
+    //Output data
+    printf("My name is %s %s, ", name, surname);
+    printf("you can call me %s.\n", nick_name);
+    printf("Gender : %c , %c\n", gender, Gender);
+    printf("Age : %d\n", age);
+    printf("GPA : %.2f\n", gpa);
+    printf("Status : %d\n", status);
+    return 0;
+}
