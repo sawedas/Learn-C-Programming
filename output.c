0bbadb69cb974f89447de14e266d18c519f54f2a
sawedas <tar.tachit@Tachits-MacBook-Air.local>
2023-12-05 10:15:00 +0700

    first commit

---
+++ output.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //stdio = standard input out put and this line import library (Header file)
+
+int main() //main is function start program
+{
+    printf("hello, world\n");
+    printf("6610402051\n");
+    printf("Student Data\n");
+    printf("Name : %s , SurName : %s\n","Tar","Tachit");
+    printf("Age : %d\n", 19);
+    printf("Gender : %c\n",'M');
+    printf("GPA = %.2f\n", 3.86);
+    printf("This is good student.");
+    return 0;
+} 
